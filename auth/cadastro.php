41d0be6f46398e6e07b71228bba3310c14dfdfff
arthurlimazx <arthur.lima99@aluno.ce.gov.br>
2026-06-08 21:27:37 -0300

    verificação de email no cadastro novamente

--- auth/cadastro.php
+++ auth/cadastro.php
@@ -44,3 +44,93 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../assets/login.css">
+    <link rel="stylesheet" href="../assets/navbar.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'popup.php'; ?>
+
+<div class="navbar-container">
+
+
+<nav class="navbar" id="navbar">
+  <div class="navbar-inner">
+ 
+    
+    <a href="../index.php" class="logo">
+      <div class="logo-icone">🏎️</div>
+      <div class="logo-text">
+        <span class="logo-title">F1 HUB</span>
+        <span class="logo-season">TEMPORADA 2026</span>
+      </div>
+    </a>
+ 
+    
+    <ul class="nav-links">
+      <li><a href="../index.php" class="active">INICIO</a></li>
+      <li><a href="../pilotos/pilotos.php">PILOTOS</a></li>
+      <li><a href="../equipe/equipes.php">EQUIPES</a></li>
+      <li><a href="../corridas/corridas.php">CALENDARIO</a></li>
+    </ul>
+    <?php if (isset($_SESSION['id'])): ?>
+            <div class="nav-user">
+                    
+                    <a href="auth/logout.php" class="nav-logout">Sair</a>
+                </div>
+            <?php else: ?>
+            <div class="nav-user">
+                <a href="login.php" class="nav-login">Entrar</a>
+                <a href="cadastro.php" class="nav-cadastro">Registrar</a>
+            </div>
+            
+            <?php endif; ?>
+    
+    
+  </div>
+</nav>
+</div>
+
+
+<div class="pagina">
+    <div class="video">
+        <video autoplay muted playsinline loop src="../assets/f1.mp4"></video>
+
+
+    </div>
+    <div class="formulario">
+        
+        
+            <form action="" method="POST">
+            <h1>CADASTRO</h1>
+
+        <div class="input-box">
+            <input type="text" name="nome" placeholder="Nome">
+        </div>
+        <div class="input-box">
+              
+            <input type="text" name="email" placeholder="Email"> 
+            
+        </div>
+        <div class="input-box">
+            <input type="password" name="senha" placeholder="Senha" >
+        </div>
+            <button type="submit" class="btn">Crie uma conta</button>
+            <div class="registro">
+               
+
+            </div>
+            </form>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
